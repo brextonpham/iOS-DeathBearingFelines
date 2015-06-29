fdc9987726c2810ca3c372555d97c7b265e90848
Brexton Pham <brextonpham@stanford.edu>
2015-06-28 23:46:27 -0400

    Initial Commit

---
+++ DeathBearingFelines/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  DeathBearingFelines
+//
+
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ DeathBearingFelines/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  DeathBearingFelines
+//
+
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
