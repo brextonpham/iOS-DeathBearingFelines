d740171fe1dc9b12409deee94cda816ea414f630
Brexton Pham <brextonpham@stanford.edu>
2015-06-29 00:13:04 -0400

    Uploading DeathBearingFelines
    - Just starting creating the game

--- DeathBearingFelines/GameScene.m
+++ DeathBearingFelines/GameScene.m
@@ -10,16 +10,25 @@
 
 @implementation GameScene
 
--(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    
-    [self addChild:myLabel];
+-(id)initWithSize:(CGSize)size {
+    if (self = [super initWithSize:size]) {
+        /* Setup your scene here */
+        
+        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        
+        SKSpriteNode *greenNode = [SKSpriteNode spriteNodeWithColor:[SKColor greenColor] size:CGSizeMake(100, 100)];
+        greenNode.position = CGPointMake(150, 150);
+        greenNode.anchorPoint = CGPointMake(0, 0);
+        [self addChild:greenNode];
+        
+        SKSpriteNode *redNode = [SKSpriteNode spriteNodeWithColor:[SKColor redColor] size:CGSizeMake(15, 15)];
+        redNode.position = CGPointMake(150, 150);
+        redNode.anchorPoint = CGPointMake(0, 0);
+        [self addChild:redNode];
+        
+        
+    }
+    return self;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -30,8 +39,6 @@
         
         SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
         
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
         sprite.position = location;
         
         SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
@@ -47,3 +54,4 @@
 }
 
 @end
+
